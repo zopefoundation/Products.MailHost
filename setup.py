1aadfb13a6bb671be8b3b1f4aa2c9937cd66db32
Hanno Schlichting <hanno@hannosch.eu>
2013-03-01 23:35:47 +0100

    indent setup arguments with four spaces

--- setup.py
+++ setup.py
@@ -14,22 +14,23 @@
 
 from setuptools import setup, find_packages
 
-setup(name='Products.MailHost',
-      version = '2.13.2dev',
-      url='http://pypi.python.org/pypi/Products.MailHost',
-      license='ZPL 2.1',
-      description="zope.sendmail integration for Zope 2.",
-      author='Zope Foundation and Contributors',
-      author_email='zope-dev@zope.org',
-      long_description=open('README.txt').read() + '\n' +
-                       open('CHANGES.txt').read(),
-      packages=find_packages('src'),
-      namespace_packages=['Products'],
-      package_dir={'': 'src'},
-      install_requires=[
+setup(
+    name='Products.MailHost',
+    version='2.13.2dev',
+    url='http://pypi.python.org/pypi/Products.MailHost',
+    license='ZPL 2.1',
+    description="zope.sendmail integration for Zope 2.",
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    long_description=open('README.txt').read() + '\n' +
+                     open('CHANGES.txt').read(),
+    packages=find_packages('src'),
+    namespace_packages=['Products'],
+    package_dir={'': 'src'},
+    install_requires=[
         'setuptools',
         'Zope2 >= 2.13.0a1',
-      ],
-      include_package_data=True,
-      zip_safe=False,
-      )
+    ],
+    include_package_data=True,
+    zip_safe=False,
+)
