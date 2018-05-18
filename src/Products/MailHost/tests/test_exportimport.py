7675d4f2aae07403db413a491673358e4c575346
Jens Vagelpohl <jens@netz.ooo>
2018-05-18 16:45:18 +0200

    - remove obsolete zcml condition, broken imports and superfluous type checks

--- src/Products/MailHost/tests/test_exportimport.py
+++ src/Products/MailHost/tests/test_exportimport.py
@@ -24,15 +24,15 @@ except:
     class BodyAdapterTestCase: pass
     class ExportImportZCMLLayer: pass
 
-_MAILHOST_BODY = """\
-<?xml version="1.0"?>
+_MAILHOST_BODY = b"""\
+<?xml version="1.0" encoding="utf-8"?>
 <object name="foo_mailhost" meta_type="Mail Host" smtp_host="localhost"
    smtp_port="25" smtp_pwd="" smtp_queue="False" smtp_queue_directory="/tmp"
    smtp_uid=""/>
 """
 
-_MAILHOST_BODY_v2 = """\
-<?xml version="1.0"?>
+_MAILHOST_BODY_v2 = b"""\
+<?xml version="1.0" encoding="utf-8"?>
 <object name="foo_mailhost" meta_type="Mail Host" smtp_host="localhost"
    smtp_port="25" smtp_pwd="" smtp_queue="True"
    smtp_queue_directory="/tmp/mailqueue" smtp_uid=""/>
@@ -45,23 +45,16 @@ class MailHostXMLAdapterTests(BodyAdapterTestCase, unittest.TestCase):
     layer = ExportImportZCMLLayer
 
     def _getTargetClass(self):
-        from Products.GenericSetup.MailHost.exportimport \
-                import MailHostXMLAdapter
+        from Products.MailHost.exportimport import MailHostXMLAdapter
 
         return MailHostXMLAdapter
 
     def _verifyImport(self, obj):
-        self.assertEqual(type(obj.smtp_host), str)
         self.assertEqual(obj.smtp_host, 'localhost')
-        self.assertEqual(type(obj.smtp_port), int)
         self.assertEqual(obj.smtp_port, 25)
-        self.assertEqual(type(obj.smtp_pwd), str)
         self.assertEqual(obj.smtp_pwd, '')
-        self.assertEqual(type(obj.smtp_uid), str)
         self.assertEqual(obj.smtp_uid, '')
-        self.assertEqual(type(obj.smtp_queue), bool)
         self.assertEqual(obj.smtp_queue, False)
-        self.assertEqual(type(obj.smtp_queue_directory), str)
         self.assertEqual(obj.smtp_queue_directory, '/tmp')
 
     def setUp(self):
@@ -93,15 +86,12 @@ class MailHostXMLAdapterTestsWithQueue(BodyAdapterTestCase, unittest.TestCase):
     layer = ExportImportZCMLLayer
 
     def _getTargetClass(self):
-        from Products.GenericSetup.MailHost.exportimport \
-                import MailHostXMLAdapter
+        from Products.MailHost.exportimport import MailHostXMLAdapter
 
         return MailHostXMLAdapter
 
     def _verifyImport(self, obj):
-        self.assertEqual(type(obj.smtp_queue), bool)
         self.assertEqual(obj.smtp_queue, True)
-        self.assertEqual(type(obj.smtp_queue_directory), str)
         self.assertEqual(obj.smtp_queue_directory, '/tmp/mailqueue')
 
     def test_body_get(self):
@@ -119,13 +109,9 @@ unittest.skipUnless(HAVE_GS, 'Products.GenericSetup not available.')
 class MailHostXMLAdapterTestsWithNoneValue(MailHostXMLAdapterTests):
 
     def _verifyImport(self, obj):
-        self.assertEqual(type(obj.smtp_host), str)
         self.assertEqual(obj.smtp_host, 'localhost')
-        self.assertEqual(type(obj.smtp_port), int)
         self.assertEqual(obj.smtp_port, 25)
-        self.assertEqual(type(obj.smtp_pwd), str)
         self.assertEqual(obj.smtp_pwd, '')
-        self.assertEqual(type(obj.smtp_uid), str)
         self.assertEqual(obj.smtp_uid, '')
 
     def setUp(self):
