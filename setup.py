b4f29646588dbd5d861a921b4a245a47c0abb085
Jens Vagelpohl <jens@netz.ooo>
2018-11-24 20:59:26 -0600

    - prevent flake8 failure [ci skip]

--- setup.py
+++ setup.py
@@ -14,6 +14,12 @@
 
 from setuptools import setup, find_packages
 
+
+def _read(fname):
+    with open(fname) as fp:
+        return fp.read()
+
+
 setup(
     name='Products.MailHost',
     version='4.3.dev0',
@@ -22,8 +28,7 @@ setup(
     description="zope.sendmail integration for Zope.",
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
-    long_description=(open('README.rst').read() + '\n' +
-                      open('CHANGES.rst').read()),
+    long_description=_read('README.rst') + '\n' + _read('CHANGES.rst'),
     packages=find_packages('src'),
     namespace_packages=['Products'],
     package_dir={'': 'src'},
