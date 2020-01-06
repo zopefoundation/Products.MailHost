c12cd2f9ea6dc6e5cd53bb3407e666cdadd532c1
Jens Vagelpohl <jens@netz.ooo>
2020-01-06 16:51:06 -0600

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def _read(fname):
 
 setup(
     name='Products.MailHost',
-    version='4.8',
+    version='4.9.dev0',
     url='https://github.com/zopefoundation/Products.MailHost',
     project_urls={
         'Issue Tracker': ('https://github.com/zopefoundation'
